122db3da957088a291e56bec1c7e19b0c2afc173
leticia18rodriguescardoso@gmail.com <leticia18rodriguescardoso@gmail.com>
2025-04-01 16:29:28 -0300

    Criado arquivo para ensinar datatypes

---
+++ aula01/concatenar.php
@@ -0,0 +1,12 @@
+<?php
+ECHO "Hello world!<br>";
+echo "Hello world!<br>";
+EcHo "Hello world!<br>";
+
+$nome = "Leticia";
+$sobrenome = "Cardoso";
+$numero = 21;
+
+echo "${nome} <h1> ${sobrenome} </h1>";
+echo $numero . "<p> " . $nome . " " . $sobrenome . "</p>";
+?>
---
+++ aula02/datatypes.php
@@ -0,0 +1,23 @@
+<?php
+    $x = "Hello World!";
+    $y = "Hello world!";
+
+    var_dump($x);
+    echo "<br>";
+    var_dump($y);
+    echo "<br>";
+
+    $x = 12.4;
+
+    var_dump($x);
+    echo "<br>";
+
+    $y = ['Leticia', 'Matheus', 7];
+
+    var_dump($y);
+    echo "<br>";
+    echo $y[1];
+
+    $meuObj -> nome = "Davi";
+    $meuObj -> idade = 32;
+?>
