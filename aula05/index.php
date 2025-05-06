27adb518b841e724def1f659f94c35915423f968
leticia cardoso <leticia18rodriguescardoso@gmail.com>
2025-05-06 16:29:56 -0300

    formulario

---
+++ aula05/index.php
@@ -0,0 +1,13 @@
+<html>
+    <body>
+
+    <form action="mensagem.php" method="POST">
+        Digite sua mensagem:
+        <input type="text" name="msg"><br>
+        Digite seu nome:
+        <input type="text" name="nome"><br>
+        <input type="submit">
+</form>
+
+</body>
+</html>
